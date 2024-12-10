00f4ba8f490a13fb288e983ec073c813b10c88e0
enis75079 <naveenislam55@gmail.com>
2024-12-09 18:42:17 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,17 @@ Naveen Islam
 
 */
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+// node that stores the customer's name and drink order
+struct Node {
+    string cusName;
+    string cusOrder;
+    Node* next;
+};
+
 int main() {
     return 0;
 }
