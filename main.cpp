e888a3f37a51476fa30b8d3dca56c4aa3c38fee4
enis75079 <naveenislam55@gmail.com>
2024-12-09 20:34:40 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -72,6 +72,10 @@ void dequeVendor(deque<Customer>& queue, const string& vendor, vector<string>& n
         cout << "No customers." << endl;
     }
 
+    if (rand() % 2 == 0) {
+        queue.push_back({names[rand() % 18], muffins[rand() % 10]});
+    }
+
     if (!queue.empty()) {
         cout << "Muffin Booth Queue: " << endl;
         for (const auto& cus : queue) {
@@ -81,28 +85,54 @@ void dequeVendor(deque<Customer>& queue, const string& vendor, vector<string>& n
 
 }
 
+void vectorVendor(vector<Customer>& queue, const string& vendor, vector<string>& names, vector<string>& bracelets) {
+    if (!queue.empty()) {
+        Customer cus = queue.front();
+        cout << "Serving: " << cus.cusName << ", Ordered: " << cus.cusOrder << endl;
+    } else {
+        cout << "No customers." << endl;
+    }
+
+    if (rand() % 2 == 0) {
+        queue.push_back({names[rand() % 18], bracelets[rand() % 10]});
+    }
+
+    if (!queue.empty()) {
+        cout << "Bracelet Booth Queue: " << endl;
+        for (const auto& cus : queue) {
+            cout << cus.cusName << " wants " << cus.cusOrder << endl;
+        }
+    }
+}
+
 // main function
 int main() {
 
     vector<string> customerNames = {"Yoshi", "Link", "Zelda", "Ganondorf", "Impa", "Peach", "Mario", "Luigi", "Bowser", "Pauline", "King Dedede", "Fox", "Ness", "Lucas", "Ash", "Pikachu", "Isabelle", "Captain Toad"};
     vector<string> coffeeNames = {"Caramel Macchiato", "Vanilla Latte", "Hazelnut Mocha", "Pumpkin Spice Latte", "Coconut Cappuccino", "Irish Cream Coffee", "Maple Pecan Latte", "Almond Milk Flat White", "Salted Caramel Cold Brew", "White Chocolate Mocha"};
     vector<string> muffinNames = {"Chocolate Chip Muffin", "Egg Muffin", "Strawberry Muffin", "Pumpkin Spice Muffin", "Banana Nut Muffin", "Rebel Muffin", "Keto Blueberry Muffin", "Apple Cinnamon Muffin", "Keto Double Chocolate Muffin", "2x2 Muffin"};
-    
+    vector<string> braceletNames = {"Beaded Bracelet", "Woven Bracelet", "Friendship Bracelet", "Charm Bracelet", "Macrame Bracelet", "Custom Name Bracelet", "Infinity Bracelet", "Heart Bracelet", "Gummy Bracelet", "Spike Bracelet"};
+
     Node* head = nullptr;
     Node* tail = nullptr;
 
     deque<Customer> muffins;
 
+    vector<Customer> bracelets;
+
     for (int i = 0; i < 3; i++) {
         addCustomer(customerNames[rand() % 18], coffeeNames[rand() % 10], head, tail);
-        muffins.push_back({customerNames[rand() % 18], muffinNames[rand() % 18]});
+        muffins.push_back({customerNames[rand() % 18], muffinNames[rand() % 10]});
+        bracelets.push_back({customerNames[rand() % 18], muffinNames[rand() % 10]});
     }
 
     for (int i = 0; i <= 9; i++) {
+        cout << endl;
         cout << "Round: " << i + 1 << endl;
 
         if (head) {
             serveCustomer(head, tail);
+            cout << endl;
         } else {
             cout << "No customers." << endl;
         }
@@ -114,6 +144,8 @@ int main() {
         queueOutput(head);
 
         dequeVendor(muffins, "Muffins Booth", customerNames, muffinNames);
+
+        vectorVendor(bracelets, "Bracelets Booth", customerNames, braceletNames);
     }
 
     return 0;
