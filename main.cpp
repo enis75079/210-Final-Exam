d0b7b619d47ff425bb2aa86ad00caeaf299ee3c9
enis75079 <naveenislam55@gmail.com>
2024-12-09 18:51:59 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -8,15 +8,20 @@ Naveen Islam
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-// node that stores the customer's name and drink order
+// function prototypes
+
+// struct for node in linked list got coffee booth queue simulation
 struct Node {
     string cusName;
     string cusOrder;
     Node* next;
 };
 
+
 int main() {
+    string customerNames[] = {"Yoshi", "Link", "Zelda", "Ganondorf", "Impa", "Peach", "Mario", "Luigi", "Bowser", "Paulina"};
     return 0;
 }
