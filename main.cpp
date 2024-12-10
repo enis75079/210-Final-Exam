c0da837d9f60c18d44a1fe78c30d474f73c07de9
enis75079 <naveenislam55@gmail.com>
2024-12-09 19:12:26 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ Naveen Islam
 using namespace std;
 
 // function prototypes
-void addCustomer(string&, string&, Node&);
 
 // struct for node in linked list got coffee booth queue simulation
 struct Node {
@@ -21,10 +20,25 @@ struct Node {
     Node* next;
 };
 
-void addCustomer(string& name, string& coffee, Node& head) {
+void addCustomer(const string& name, const string& coffee, Node*& head) {
     Node* newNode = new Node{name, coffee, nullptr};
     if (!head) {
         head = newNode;
+    } else {
+        Node* current = head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+void queueOutput(Node* head) {
+    if (head != nullptr) {
+        Node* current = head;
+        while (current) {
+            cout << current->cusName << " ordered " << current->cusOrder << endl;
+        }
     }
 }
 
@@ -32,5 +46,13 @@ int main() {
     string customerNames[] = {"Yoshi", "Link", "Zelda", "Ganondorf", "Impa", "Peach", "Mario", "Luigi", "Bowser", "Pauline", "King Dedede", "Fox", "Ness", "Lucas", "Ash", "Pikachu", "Isabelle", "Captain Toad"};
     string coffeeNames[] = {"Caramel Macchiato", "Vanilla Latte", "Hazelnut Mocha", "Pumpkin Spice Latte", "Coconut Cappuccino", "Irish Cream Coffee", "Maple Pecan Latte", "Almond Milk Flat White", "Salted Caramel Cold Brew", "White Chocolate Mocha"};
     
+    Node* head = nullptr;
+
+    for (int i = 0; i < 3; i++) {
+        addCustomer(customerNames[i], coffeeNames[i], head);
+    }
+
+    queueOutput(head);
+
     return 0;
 }
